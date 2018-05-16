05a6d1d1d4ba59a6f967742aaa76167092f9f13d
Cal Paterson <cal@calpaterson.com>
2018-05-16 19:45:53 -0400

    Bouncing puck

--- breakout/main.py
+++ breakout/main.py
@@ -4,7 +4,7 @@ from logging import getLogger, INFO, basicConfig
 import pygame
 import pygame.locals
 
-logger = getLogger(__name__)
+logger = getLogger("breakout")
 
 display_surface = None
 
@@ -27,9 +27,69 @@ class Paddle(pygame.sprite.Sprite):
         midtop_y = max(min(mouse_position[0], max_y), min_y)
         self.rect.midtop = (midtop_y, self.midtop_x)
 
+
+class Puck(pygame.sprite.Sprite):
+    def __init__(self, paddle):
+        pygame.sprite.Sprite.__init__(self)
+        self.white = (254, 254, 254)
+        self.width = 480/50
+        self.height = 480/50
+        self.image = pygame.Surface((self.width, self.height)).convert()
+        self.image.fill(self.white)
+
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = paddle.rect.midtop
+
+        self.paddle = paddle
+
+        self.served = False
+        self.x_direction = -1
+        self.y_direction = -1
+        self.last_contact = "paddle"
+
+    def update(self):
+        on_ceiling = self.rect.top <= 0
+        on_wall = any([
+            self.rect.left <= 0,
+            self.rect.right >= 640
+        ])
+        on_paddle = all([
+            self.rect.colliderect(self.paddle.rect),
+            self.paddle.rect.centery > self.rect.centery,
+            self.last_contact != "paddle",
+        ])
+        on_floor = self.rect.top > 480
+
+        if on_floor:
+            logger.info("on_floor")
+            self.served = False
+            self.x_direction = -1
+            self.y_direction = -1
+            self.last_contact = "paddle"
+        elif on_ceiling:
+            logger.info("on_ceiling")
+            self.x_direction *= -1
+            self.last_contact = "ceiling"
+        elif on_wall:
+            logger.info("on_wall")
+            self.y_direction *= -1
+            self.last_contact = "wall"
+        elif on_paddle:
+            logger.info("on_paddle")
+            self.x_direction *= -1
+            self.last_contact = "paddle"
+
+        if not self.served:
+            self.rect.midbottom = self.paddle.rect.midtop
+        else:
+            self.rect.move_ip(2 * self.y_direction, 3 * self.x_direction)
+
+
 def main():
     global display_surface
     pygame.init()
+
+    logger.info("driver = %s", pygame.display.get_driver())
 
     pygame.display.set_caption("Breakout")
 
@@ -39,24 +99,35 @@ def main():
     background.fill((0, 0, 0))
 
     paddle = Paddle()
-    allsprites = pygame.sprite.RenderPlain((paddle,))
+    puck = Puck(paddle)
+    allsprites = pygame.sprite.RenderUpdates((paddle, puck))
 
     clock = pygame.time.Clock()
 
+    loop_index = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 going = False
                 sys.exit(1)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                logger.info("mouse click")
+                puck.served = True
 
         allsprites.update()
 
         display_surface.blit(background, (0, 0))
-        allsprites.draw(display_surface)
-        pygame.display.flip()
+        updated_rects = allsprites.draw(display_surface)
+        pygame.display.update(updated_rects)
 
         clock.tick(60)
+
+        if loop_index > (60 * 5):
+            loop_index = 0
+            logger.info("FPS = %d", clock.get_fps())
+        else:
+            loop_index += 1
 
 
 if __name__ == "__main__":
